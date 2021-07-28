0e4dfafe460f18fbc6a65e1f82ae6da24f226a3c
JamesGallant <jamesgallant17@gmail.com>
2021-07-28 20:53:46 +0200

    testing new CI

--- accounts/src/accounts/settings.py
+++ accounts/src/accounts/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'accounts.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": develop_configuration.get("sql_engine", "django.db.backends.sqlite3"),
-        "NAME": develop_configuration.get("sql_database", os.path.join(BASE_DIR, "db.sqlite3")),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
         "USER": os.environ.get("SQL_USER", None),
         "PASSWORD": os.environ.get("SQL_PASSWORD", None),
         "HOST": os.environ.get("SQL_HOST", "localhost"),
