b3cc03bab0a95d6b9b471f02437b799e07ee88e6
JamesGallant <jamesgallant17@gmail.com>
2021-08-30 21:43:18 +0200

    testing ci

--- service_frontend/jest.config.js
+++ service_frontend/jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  "modulePaths": [
+    "<rootDir>"
+  ],
   testMatch: ['src/__tests__/*.ts?(x)']
 };
---
+++ service_frontend/src/modules/__tests__/authenticate.test.tsx
@@ -0,0 +1,14 @@
+import authenticate from "../authenticate";
+import type { AxiosError } from '../../types/types';
+
+jest.mock("axios");
+
+describe("Testing authentication", () => {
+    beforeEach(() => {
+
+    });
+    
+    afterEach(() => {
+        jest.clearAllMocks();
+    })
+})
