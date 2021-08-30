a7308013d52f2f54b8ab11bc11b8bd46bb17f738
JamesGallant <jamesgallant17@gmail.com>
2021-08-30 21:28:07 +0200

    test

--- service_frontend/src/components/helper/__tests__/privateRoute.test.tsx
+++ service_frontend/src/components/helper/__tests__/privateRoute.test.tsx
@@ -1,7 +1,6 @@
 import { render} from '@testing-library/react';
 import { BrowserRouter, Switch } from "react-router-dom";
 
-
 import configuration from '../../../utils/config';
 import DashboardView from '../../../views/dashboardView';
 import PrivateRoute from "../privateRoute";
