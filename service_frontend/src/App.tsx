90dcdb11fcaf580f725e4d5d2c13f5581db36722
JamesGallant <jamesgallant17@gmail.com>
2021-11-02 08:34:22 +0100

    added themeProvider

--- service_frontend/src/App.tsx
+++ service_frontend/src/App.tsx
@@ -1,27 +1,31 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { ThemeProvider, createMuiTheme, makeStyles } from "@material-ui/core/styles";
 
 import configuration from "./utils/config";
 import PrivateRoute from "./components/helper/privateRoute";
 import views from "./views";
 
-
+const theme = createMuiTheme();
 const App = (): JSX.Element => {
 	return(
-		<BrowserRouter>
-			<Switch>
-				<Route exact path={ configuration["url-home"] } component={views.HomeView}/>
-				<Route exact path={ configuration["url-login"] } component={views.LoginView}/>
-				<Route exact path={ configuration["url-logout"] } component={views.LogoutView}/>
-				<Route exact path={ configuration["url-register"] } component={views.RegisterView}/>
-				<Route path={ configuration["url-accountCreated"] } exact component={views.AccountCreatedView}/>
-				<Route path= { configuration["url-acitvateAccount"] } exact component={views.AccountActivationView}/>
-				<Route exact path={ configuration["url-resetPassword"] } component={views.ResetPassword}/>
-				<Route exact path={ configuration["url-resetPasswordConfirm"] } component={views.ResetPasswordConfirm}/>
-				<Route exact path={ configuration["url-resetPasswordEmailSent"]} component={views.ResetPasswordEmailSent}/>
-				<PrivateRoute path={ configuration["url-dashboard"] } component={views.DashboardView}/>
-			</Switch>
-		</BrowserRouter>
+		<ThemeProvider theme={theme}>
+			<BrowserRouter>
+				<Switch>
+					<Route exact path={ configuration["url-home"] } component={views.HomeView}/>
+					<Route exact path={ configuration["url-login"] } component={views.LoginView}/>
+					<Route exact path={ configuration["url-logout"] } component={views.LogoutView}/>
+					<Route exact path={ configuration["url-register"] } component={views.RegisterView}/>
+					<Route path={ configuration["url-accountCreated"] } exact component={views.AccountCreatedView}/>
+					<Route path= { configuration["url-acitvateAccount"] } exact component={views.AccountActivationView}/>
+					<Route exact path={ configuration["url-resetPassword"] } component={views.ResetPassword}/>
+					<Route exact path={ configuration["url-resetPasswordConfirm"] } component={views.ResetPasswordConfirm}/>
+					<Route exact path={ configuration["url-resetPasswordEmailSent"]} component={views.ResetPasswordEmailSent}/>
+					<PrivateRoute path={ configuration["url-dashboard"] } component={views.DashboardView}/>
+				</Switch>
+			</BrowserRouter>
+		</ThemeProvider>
+
 	);
 };
 
