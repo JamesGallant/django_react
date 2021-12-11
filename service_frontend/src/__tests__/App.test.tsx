5587e8f5acb82ccec9af1f8d15f92994c90636ad
JamesGallant <jamesgallant17@gmail.com>
2021-12-11 15:26:57 +0100

    fixed current tests

--- service_frontend/src/__tests__/App.test.tsx
+++ service_frontend/src/__tests__/App.test.tsx
@@ -1,5 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "@testing-library/react";
+import { createMemoryHistory } from "history";
+import { Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "../store/store";
 import App from "../App";
 
 /**
@@ -11,6 +15,12 @@ import App from "../App";
 
 // smoke test
 it("App component renders", () => {
-	const div = document.createElement("div");
-	ReactDOM.render(<App />, div);
+	const history = createMemoryHistory();
+	render(
+		<Provider store={store}>
+			<Router history={history}>
+				<App />
+			</Router>
+		</Provider>
+	);
 });
--- service_frontend/src/components/dashboard/__tests__/SettingsAppearance.test.tsx
+++ service_frontend/src/components/dashboard/__tests__/SettingsAppearance.test.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "../../../store/store";
 
 import SettingsAppearance from "../SettingsAppearance";
 
 describe("Testing profile settings", () => {
 	it("Mounts", () => {
-		render(<SettingsAppearance />);
+		render(
+			<Provider store={store}>
+				<SettingsAppearance />
+			</Provider>
+		);
 	});
 
 });
