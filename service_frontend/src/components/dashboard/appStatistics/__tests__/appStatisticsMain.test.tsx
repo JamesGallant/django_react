5d4d834385133aafdeb8ec848e2db6a284971701
JamesGallant <jamesgallant17@gmail.com>
2022-01-03 09:48:09 +0100

    fixed test import

--- service_frontend/src/components/dashboard/appStatistics/__tests__/appStatisticsMain.test.tsx
+++ service_frontend/src/components/dashboard/appStatistics/__tests__/appStatisticsMain.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
 
-import AppStatisticsMain from "../AppStatisticsMain";
+import AppStatisticsMain from "../appStatisticsMain";
 
 describe("Testing the entry component of the statistics application", () => {
 	it("should mount", () => {
